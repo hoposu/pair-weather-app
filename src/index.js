dc1b950dcd00463e7ccbf2446d2e1ba383b18174
Dilly Sanborn-Marsh <csanbornmarsh@gmail.com>
2023-02-16 12:48:29 -0800

    Updated html, js, css files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+alert("hi");
+
+/*List of steps
+1.  Link CSS & JS
+2. Add boostrap in head: https://getbootstrap.com/docs/4.3/getting-started/introduction/, wrap body in a div container (div.container+tab)
+
+*/
