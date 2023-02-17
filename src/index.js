60a7cc1ef790ee1b23d4b8921add26750501f9b8
Dilly Sanborn-Marsh <csanbornmarsh@gmail.com>
2023-02-17 14:55:57 -0800

    styled main elements of the page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /*List of steps
 1.  Link CSS & JS
 2. Add boostrap in head: https://getbootstrap.com/docs/4.3/getting-started/introduction/, wrap body in a div container (div.container+tab)
-
+3. Make sure you put styles AFTER the bootstrap link so that bootstrap has precedence
+4. Break down your UI into divs
 */
