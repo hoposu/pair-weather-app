b8f294d08c2182ef3003751f14af365daeecfb47
Dilly Sanborn-Marsh <csanbornmarsh@gmail.com>
2023-02-17 13:25:42 -0800

    Changes on thursday 2/17

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-alert("hi");
-
 /*List of steps
 1.  Link CSS & JS
 2. Add boostrap in head: https://getbootstrap.com/docs/4.3/getting-started/introduction/, wrap body in a div container (div.container+tab)
